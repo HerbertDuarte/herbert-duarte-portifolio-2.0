ede5c89c766e1cf2ed5f2940c50817f1ff757b0d
herberttivic <herbert@tivic.com.br>
2025-04-11 00:47:20 -0300

    feat: info page

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,14 @@ import HomePage from "./pages/HomePage.vue";
 import KnowledgesPage from "./pages/KnowledgesPage.vue";
 import WorksPage from "./pages/WorksPage.vue";
 import FooterPage from "./pages/FooterPage.vue";
+import InfoPage from "./pages/InfoPage.vue";
 </script>
 
 <template>
   <HeaderComponent />
   <div class="flex flex-col items-stretch justify-start h-container">
     <HomePage />
+    <InfoPage />
     <WorksPage />
     <KnowledgesPage />
     <div class="flex-1 bg-zinc-50"></div>
---
+++ src/pages/InfoPage.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import ContainerComponent from "../components/ContainerComponent.vue";
+
+function goToRepos() {
+  window.open("https://github.com/HerbertDuarte?tab=repositories", "_blank");
+}
+</script>
+
+<template>
+  <ContainerComponent theme="dark">
+    <div
+      class="flex font-serif gap-16 flex-wrap items-center justify-around py-2"
+    >
+      <div class="text-center space-y-2">
+        <p class="text-4xl sm:text-5xl"><span class="text-3xl">+</span>3</p>
+        <p class="italic">
+          Anos de <br />
+          experiência
+        </p>
+      </div>
+
+      <div @click="goToRepos" class="text-center space-y-2 cursor-pointer">
+        <p class="text-4xl sm:text-5xl"><span class="text-3xl">+</span>65</p>
+        <p class="italic">Repositórios <br />públicos</p>
+      </div>
+
+      <div class="text-center space-y-2">
+        <p class="text-4xl sm:text-5xl"><span class="text-3xl">+</span>20h</p>
+        <p class="italic">
+          Cursos <br />
+          ministrados
+        </p>
+      </div>
+    </div>
+  </ContainerComponent>
+</template>
